3da0237f468cddb9c88e4d30edfc68317a8e8906
Konstantinos Katserelis <45661771+its-kos@users.noreply.github.com>
2022-04-27 21:38:49 +0300

    first commit

---
+++ src/app/IndexSearcher.java
@@ -0,0 +1,63 @@
+package app;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.core.KeywordAnalyzer;
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.similarities.BM25Similarity;
+import org.apache.lucene.search.similarities.ClassicSimilarity;
+import org.apache.lucene.store.FSDirectory;
+
+import java.nio.file.Paths;
+
+public class IndexSearcher {
+    public static ScoreDoc[] search(String query, int k){
+        try{
+            String indexLocation = ("index"); //define where the index is stored
+            String field = "contents"; //define which field will be searched
+
+            //Access the index using indexReaderFSDirectory.open(Paths.get(index))
+            IndexReader indexReader = DirectoryReader.open(FSDirectory.open(Paths.get(indexLocation))); //IndexReader is an abstract class, providing an interface for accessing an index.
+
+            //Creates a searcher searching the provided index, Implements search over a single IndexReader.
+            org.apache.lucene.search.IndexSearcher indexSearcher = new org.apache.lucene.search.IndexSearcher(indexReader);
+
+            //indexSearcher.setSimilarity(new BM25Similarity());
+            indexSearcher.setSimilarity(new ClassicSimilarity());
+
+            // define which analyzer to use for the normalization of user's query
+            //Analyzer analyzer = new EnglishAnalyzer();
+            Analyzer analyzer = new StandardAnalyzer();
+            //Analyzer analyzer = new SimpleAnalyzer();
+            //Analyzer analyzer = new KeywordAnalyzer();
+
+            // create a query parser on the field "contents"
+            QueryParser parser = new QueryParser(field, analyzer);
+
+            Query res = parser.parse(QueryParser.escape(query));
+            System.out.println("Searching for: " + res.toString(field));
+
+            // search the index using the indexSearcher
+            TopDocs results = indexSearcher.search(res, k);
+
+            ScoreDoc[] hits = results.scoreDocs;
+            System.out.println("Test: " + hits[0]);
+            //Close indexReader
+            indexReader.close();
+
+            return hits;
+
+        } catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ src/app/LuceneApp.java
@@ -0,0 +1,41 @@
+package app;
+
+import org.apache.lucene.search.ScoreDoc;
+import utils.IO;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class LuceneApp {
+    public static void main(String args[]){
+        try {
+            Indexer.index();
+            String txt_file = IO.ReadEntireFileIntoAString(".\\IR2022\\queries.txt");
+            String[] temp = txt_file.split("///");
+            String[] queries = Arrays.copyOf(temp, temp.length - 1);
+            System.out.println("Read: "+ queries.length + " queries");
+
+            int[] Ks = new int[] {20, 30, 50};
+
+            for (int k:Ks){
+                try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(".\\IR2022\\results\\results-k" + k + ".txt"), StandardCharsets.UTF_8))) {
+                    for (String query:queries) {
+                        String queryId = query.trim().split("\n")[0].trim();
+                        String text = query.trim().split("\n")[1].trim();
+                        ScoreDoc[] hits = IndexSearcher.search(text, k);
+
+                        for (int i = 1; i < hits.length; i++) {
+                            writer.write("Q" + String.format("%02d", Integer.parseInt(queryId.substring(1))) + " Q0 " + hits[i].doc + " 0 " + Float.parseFloat(String.valueOf(hits[i].score)) + " myRun" + "\n");
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
