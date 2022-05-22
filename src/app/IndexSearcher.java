1dc2284497cf36e6ad85df01e8b006cd82c0fd92
marina-sam14 <marina-samb14@gmail.com>
2022-05-22 14:09:47 +0300

    Added jar library and started MoreLikeThis

--- src/app/IndexSearcher.java
+++ src/app/IndexSearcher.java
@@ -1,44 +1,66 @@
 package app;
 
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.core.KeywordAnalyzer;
-import org.apache.lucene.analysis.core.SimpleAnalyzer;
-import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.search.similarities.BM25Similarity;
+import org.apache.lucene.search.similar.MoreLikeThis;
 import org.apache.lucene.search.similarities.ClassicSimilarity;
 import org.apache.lucene.store.FSDirectory;
-
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 import java.nio.file.Paths;
 
 public class IndexSearcher {
-    private static String indexLocation = ("index");
-    private static String field = "contents";
+    private static final String indexLocation = ("index");
+    private static final String field = "contents";
+
+
     public static TopDocs search(String query, int k){
         try{
             IndexReader indexReader = DirectoryReader.open(FSDirectory.open(Paths.get(indexLocation)));
             org.apache.lucene.search.IndexSearcher indexSearcher = new org.apache.lucene.search.IndexSearcher(indexReader);
 
-            Analyzer analyzer = new MyAnalyzer();
 
+            Analyzer analyzer = new StandardAnalyzer();
+            MoreLikeThis mlt = new MoreLikeThis(indexReader);
+            mlt.setMinTermFreq(0);
+            mlt.setMinDocFreq(0);
+            mlt.setFieldNames(new String[]{"docid", "content"});
+            mlt.setAnalyzer(analyzer);
+
+            Reader sReader = new StringReader(query);
+//            Query query = mlt.like(sReader, null);
+
+            Query res = mlt.like(sReader);
             indexSearcher.setSimilarity(new ClassicSimilarity());
-
-            // create a query parser on the field "contents"
-            QueryParser parser = new QueryParser(field, analyzer);
-
-            Query res = parser.parse(QueryParser.escape(String.valueOf(query)));
-            System.out.println("Searching for: " + res.toString(field));
-
-            // search the index using the indexSearcher
             TopDocs results = indexSearcher.search(res, k);
+
+            for ( ScoreDoc scoreDoc : results.scoreDocs ) {
+                Document aSimilar = indexSearcher.doc( scoreDoc.doc );
+                String similarTitle = aSimilar.get("docid");
+                String similarContent = aSimilar.get("content");
+
+                System.out.println("====similar finded====");
+                System.out.println("title: "+ similarTitle);
+                System.out.println("content: "+ similarContent);
+            }
+
+//            indexSearcher.setSimilarity(new ClassicSimilarity());
+//
+//            // create a query parser on the field "contents"
+//            QueryParser parser = new QueryParser(field, analyzer);
+//
+//            Query res = parser.parse(QueryParser.escape(String.valueOf(query)));
+//            System.out.println("Searching for: " + res.toString(field));
+//
+//            // search the index using the indexSearcher
+//            TopDocs results = indexSearcher.search(res, k);
 
             //Close indexReader
             indexReader.close();
